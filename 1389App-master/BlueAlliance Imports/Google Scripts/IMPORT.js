c670a2c1603203c13bb8bb72a8591d2e4dff10b8
EvanB <evanb@ECBs-MBP.home>
2017-10-05 20:08:33 -0400

    Removed unnecessary files. Find them over on scoutwebapp

--- 1389App-master/BlueAlliance Imports/Google Scripts/IMPORT.js
+++
@@ -1,7 +0,0 @@
-
-function getData() {
-  var queryString = Math.random();
-  var cellFunction = '=IMPORTHTML("https://www.thebluealliance.com/event/2017chcmp#results?' + queryString + '","table",1)';
-
-  SpreadsheetApp.getActiveSheet().getRange('<importcell>').setValue(cellFunction);
-}
